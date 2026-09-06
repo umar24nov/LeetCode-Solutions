85192c7b3faaa95329ce74d33bf4a084338b37bf
Mohammad Umar <umar24nov@gmail.com>
2026-09-06 07:15:45 +0530

    Time: 0 ms (100%), Space: 23.7 MB (82.7%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -2,12 +2,17 @@ class Solution {
 public:
     void moveZeroes(vector<int>& arr) {
         int n = arr.size();
-        for(int i = 0; i < n - 1; i++){
-            for(int j = 0; j < n - i - 1; j++){
-                if(arr[j] == 0){
-                    swap(arr[j], arr[j+1]);
-                }
+
+        int j = 0;
+        for(int i = 0; i < n; i++){
+            if(arr[i] != 0){
+                arr[j] = arr[i];
+                j++;
             }
+        }
+
+        for(int i = j; i < n; i++){
+            arr[i] = 0;
         }
     }
 };
