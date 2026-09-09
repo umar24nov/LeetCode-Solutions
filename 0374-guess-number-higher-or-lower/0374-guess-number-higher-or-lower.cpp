528778270e23468ef4205897697a598f2c250a97
Mohammad Umar <umar24nov@gmail.com>
2026-09-10 01:26:29 +0530

    Time: 2 ms (56.48%), Space: 8 MB (6.66%) - LeetHub

---
+++ 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int guessNumber(int n) {
+
+        int low = 1;
+        int high = n;
+
+        while(low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            if(guess(mid) == 0) {
+                return mid;
+            }
+            else if(guess(mid) == -1) {
+                high = mid - 1;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+};
