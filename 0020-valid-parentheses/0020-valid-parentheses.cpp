8817e99fa1ad9b7d24d1993ed34d0b687bb3c7ca
Mohammad Umar <umar24nov@gmail.com>
2026-09-24 11:55:26 +0530

    Time: 3 ms (9.3%), Space: 8.9 MB (36.96%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        
+        int n = s.length();
+
+        stack<char> st;
+
+        for(int i = 0; i < n; i++){
+            if(s[i] == '(' || s[i] == '[' || s[i] == '{') st.push(s[i]);
+
+            else{
+
+                if(st.empty()) return false;
+
+                if(s[i] == ')' && st.top() != '(') return false;
+                if(s[i] == ']' && st.top() != '[') return false;
+                if(s[i] == '}' && st.top() != '{') return false;
+
+                st.pop();
+            }
+        }
+
+        return st.empty();
+    }
+};
